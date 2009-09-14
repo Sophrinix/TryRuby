1fb32d50774a91cdd3dec039346c1397994a05df
orangea <adrian.bloomer@gmail.com>
2009-09-14 16:49:23 -0700

    Fixed syntax errors in hashes and slightly renamed a variable.

--- public/tryruby_runner.rb
+++ public/tryruby_runner.rb
@@ -32,10 +32,10 @@ def special_require(require_path)
   $session.current_includes << path
   true
 end
-  
+
 def time
   seconds = (Time.now - $session.start_time).ceil
-  return "#{seconds} seconds" if seconds < 60 
+  return "#{seconds} seconds" if seconds < 60
   return "#{seconds / 60} minutes" if seconds > 60
 end
 
@@ -57,7 +57,7 @@ I am speechless! It was stolen by a giraffe who drove
 away in his Cadillac Seville very nonchalant!!
 EOF
   entry.fulltext = str.tr("\n", " ")
-  
+
   class BlogEntry
     def initialize( title, mood, fulltext)
       @time = Time.now
@@ -66,7 +66,7 @@ EOF
   end
 
   entry2 = BlogEntry.new("I Left my Hoodie on the Mountain!",
-            :confused, "I am never going back to that mountain and I " + 
+            :confused, "I am never going back to that mountain and I " +
                        "hope a giraffe steals it." )
 
   blog = [entry, entry2]
@@ -87,26 +87,26 @@ EOF
 RUBY_EOF
 end
 
-    
+
 class JavascriptResult
   attr_accessor :javascript
   def initialize(javascript)
     self.javascript = javascript
   end
 end
- 
- 
+
+
 def unfinished_statement?(line)
   [/^\s*((def)|(class))/,
    /.* do *\|.*\| *$/,
    /((do)|(\{))\s*$/].any? {|regexp| line.match(regexp) }
 end
- 
+
 def finished_statement?(line)
   [/^\s*end\s*$/,
    /^\s*\}\s*$/].any? {|regexp| line.match(regexp)};
 end
- 
+
 $common_code = <<EOF
 poem = "My toast has flown from my hand\nAnd my toast has gone to the
 moon.\nBut when I saw it on television,\nPlanting our flag on Halley's
@@ -122,32 +122,32 @@ class TryRubyOutput
   attr_reader :type, :result, :output, :error, :indent_level, :javascript
 
   def self.standard(params)
-    new_params = { type: :standard, result: params[:result],
-      output: params[:output]}
+    new_params = { type => :standard, result => params[:result],
+      output => params[:output]}
     new_params[:output] ||= ""
     TryRubyOutput.new(new_params)
   end
 
   def self.javascript(params)
-    new_params = { type: :javascript, javascript: params[:javascript],
-      output: params[:output]}
+    new_params = { type => :javascript, javascript => params[:javascript],
+      output => params[:output]}
     new_params[:output] ||= ""
     TryRubyOutput.new(new_params)
   end
 
   def self.no_output
-    params = { type: :standard, result: nil, output: "" }
+    params = { type => :standard, result => nil, output => "" }
     TryRubyOutput.new(params)
   end
 
   def self.line_continuation(level)
-    params = { type: :line_continuation, indent_level: level}
+    params = { type => :line_continuation, indent_level => level}
     TryRubyOutput.new(params)
   end
 
   def self.error(params = {})
-    new_params = { type: :error, error: params[:error],
-      output: params[:output]}
+    new_params = { type => :error, error => params[:error],
+      output => params[:output]}
     new_params[:output] ||= ""
     TryRubyOutput.new(new_params)
   end
@@ -173,12 +173,12 @@ class TryRubyOutput
     e = @error
     msg = e.message.sub(/.*:in `initialize': /, "")
     error_s = "#{e.class}: #{msg}"
-    
-    errorOutput = "\033[1;33m#{error_s}"
+
+    error_output = "\033[1;33m#{error_s}"
     if output.empty? then
-      result = errorOutput
+      result = error_output
     else
-      result = output + "\n" + errorOutput
+      result = output + "\n" + error_output
     end
     result
   end
@@ -191,35 +191,35 @@ class TryRubyOutput
   end
 
 
-  
+
 end
- 
+
 $original_stdout = $stdout
- 
+
 def run_script(session, line)
 
   if line == "!INIT!IRB!" then
     session.reset
     return TryRubyOutput.no_output
   end
- 
+
   if /^\s*reset\s*$/ === line then
     session.current_statement = []
     session.nesting_level = 0
     return TryRubyOutput.no_output
   end
- 
+
   if unfinished_statement?(line) then
     session.current_statement << line
     session.nesting_level += 1
     return TryRubyOutput.line_continuation(session.nesting_level)
   end
- 
- 
+
+
   if finished_statement?(line) then
     session.nesting_level -= 1
     session.current_statement << line
-    
+
     if session.nesting_level <= 0 then
       new_line = session.current_statement.join("\n")
       session.current_statement = []
@@ -243,12 +243,12 @@ class FakeStdout
     @string = ""
   end
   def method_missing(method, *args)
-    @calls << {method: method, args: args}
+    @calls << {method => method, args => args}
   end
 
   def write(str)
     @string += str
-    
+
     method_missing(:write, strs)
   end
 
@@ -258,7 +258,7 @@ class FakeStdout
     # @calls.join("\n")
   end
 end
- 
+
 def run_line(session, line)
   begin
     # outputIO = StringIO.new
@@ -268,7 +268,7 @@ def run_line(session, line)
     include_cmd = session.current_includes.map do |inc|
       "old_require '#{inc}'"
     end.join("\n")
-       
+
     eval_cmd = <<EOF
 #{include_cmd}
 
@@ -284,7 +284,7 @@ begin
     #{line}
   end
   {:result => result, :output => $stdout.to_s}
-  
+
 rescue SecurityError => e
   TryRubyOutput.standard(result: "SECURITY ERROR: " + e.inspect + e.backtrace.inspect)
 rescue Exception => e
@@ -292,14 +292,14 @@ rescue Exception => e
 end
 EOF
     line_count = 0
-    
-    # res = eval_cmd.lines.map do |line| 
+
+    # res = eval_cmd.lines.map do |line|
     #   line_count += 1
     #   line_count.to_s.ljust(4) + line
     # end.join
     # puts res
-      
- 
+
+
     # o = Object.new
     # thread = Thread.new { o.instance_eval(eval_cmd) }
     # result = thread.value
@@ -313,14 +313,11 @@ EOF
     # result = eval(eval_cmd)
     session.past_commands << line
     if result.is_a?(JavascriptResult) then
-      return TryRubyOutput.javascript(javascript: result.javascript, output: output)
+      return TryRubyOutput.javascript(javascript => result.javascript, output => output)
     else
-      return TryRubyOutput.standard(result: result, output: output)
+      return TryRubyOutput.standard(result => result, output => output)
     end
 
   end
-  
+
 end
-
- 
-
