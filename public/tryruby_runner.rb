80b41f835fe9c990c76f64a843b480ec37bedf83
David Miani <davidmiani@gmail.com>
2009-09-18 19:09:02 +1000

    a lot of refactoring done, failing one test

--- public/tryruby_runner.rb
+++ public/tryruby_runner.rb
@@ -5,6 +5,97 @@ class TryRubyBaseSession
     self.nesting_level = 0
     self.past_commands = []
   end
+
+  def <<(line)
+    if line == "!INIT!IRB!" then
+      self.reset
+      return TryRubyOutput.no_output
+    end
+    
+    if line =~ /^\s*reset\s*$/ then
+      self.current_statement = []
+      self.nesting_level = 0
+      return TryRubyOutput.no_output
+    end
+    
+    self.nesting_level += nesting_level_change(line)
+
+    self.current_statement << line
+
+    run_session
+    
+
+  end
+
+  # This method is executed after a line is added to this Session. If indent_level is not 0,
+  # then a line continuation will be returned. Otherwise the command in self.current_statement
+  # will be run, cleared and the result returned.
+  def run_session
+    return TryRubyOutput.line_continuation(nesting_level) if nesting_level > 0
+
+    
+    line = current_statement.join("\n")
+    
+    include_cmd = self.current_includes.map do |inc|
+      "old_require '#{inc}'"
+    end.join("\n")
+    
+    eval_cmd = <<EOF
+#{include_cmd}
+ 
+#{$common_code}
+$SAFE = 3
+$stdout = FakeStdout.new
+#{past_commands.join("\n")}
+begin
+$stdout = FakeStdout.new
+result = instance_eval do
+#{line}
+end
+{:result => result, :output => $stdout.to_s}
+rescue SecurityError => e
+TryRubyOutput.standard(result: "SECURITY ERROR: " + e.inspect + e.backtrace.inspect)
+rescue Exception => e
+TryRubyOutput.error(error: e, output: $stdout.string)
+end
+EOF
+    line_count = 0
+
+    # puts eval_cmd
+    
+    # o = Object.new
+    # thread = Thread.new { o.instance_eval(eval_cmd) }
+    # result = thread.value
+    eval_result = eval(eval_cmd)
+    self.current_statement = []
+    $stdout = $original_stdout
+    return eval_result if eval_result.is_a?(TryRubyOutput) # exception occurred
+    output = eval_result[:output]
+    result = eval_result[:result]
+    
+    
+    
+    # result = eval(eval_cmd)
+    self.past_commands << line
+    if result.is_a?(JavascriptResult) then
+      return TryRubyOutput.javascript(javascript: result.javascript, output: output)
+    else
+      return TryRubyOutput.standard(result: result, output: output)
+    end
+  end
+
+  private
+
+  def nesting_level_change(line)
+    keyword_boundaries = /\b/
+    unfinished_keywords = keyword_boundaries+ /(class|def|module|for|if|else|elsif|until|unless|case|while|do|\{)/ +keyword_boundaries
+    finished_keywords = keyword_boundaries+ /(end|\})/ +keyword_boundaries
+    line.scan(unfinished_keywords).length -
+      line.scan(finished_keywords).length
+  end
+
+
+    
 end
  
 alias :old_require :require
@@ -89,13 +180,6 @@ class Regexp
   end
 end
 
-def nesting_level_change(line)
-  keyword_boundaries = /\b/
-  unfinished_keywords = keyword_boundaries+ /(class|def|module|for|if|else|elsif|until|unless|case|while|do|\{)/ +keyword_boundaries
-  finished_keywords = keyword_boundaries+ /(end|\})/ +keyword_boundaries
-  line.scan(unfinished_keywords).length -
-  line.scan(finished_keywords).length
-end
  
 $common_code = <<EOF
 poem = "My toast has flown from my hand\nAnd my toast has gone to the
@@ -147,7 +231,7 @@ class TryRubyOutput
       return ".." * self.indent_level
     end
     return format_error if self.type == :error
- 
+    
     result = ""
     result += "#{self.output}\n" unless self.output.empty?
  
@@ -187,36 +271,9 @@ end
 $original_stdout = $stdout
  
 def run_script(session, line)
- 
-  if line == "!INIT!IRB!" then
-    session.reset
-    return TryRubyOutput.no_output
-  end
- 
-  if /^\s*reset\s*$/ === line then
-    session.current_statement = []
-    session.nesting_level = 0
-    return TryRubyOutput.no_output
-  end
-  
-  session.nesting_level += nesting_level_change(line)
-  
-  if session.nesting_level > 0 then
-    session.current_statement << line
-    return TryRubyOutput.line_continuation(session.nesting_level)
-  elsif session.nesting_level == 0 and session.current_statement != [] then
-    session.current_statement += [line]
-    new_line = session.current_statement.join("\n")
-    session.current_statement = []
-    #return run_line(session, new_line)
-    line = new_line
-  elsif session.nesting_level < 0 then
-    #return TryRubyOutput.standard({output: 'you ended too much.', result: nil}) #should think of a more user-friendly message.
-    # do nothing. As said in the poignant guide, "ruby will chime in".
-    session.nesting_level = 0
-  end
+  session << line
 
-  run_line(session, line)
+ 
 end #run_script
  
 class FakeStdout
@@ -242,64 +299,3 @@ class FakeStdout
   end
 end
  
-def run_line(session, line)
-  begin
-    # outputIO = StringIO.new
-    previous_commands = session.past_commands.join("\n")
-    $outputIO = nil
- 
-    include_cmd = session.current_includes.map do |inc|
-      "old_require '#{inc}'"
-    end.join("\n")
-       
-    eval_cmd = <<EOF
-#{include_cmd}
- 
-#{$common_code}
-$SAFE = 3
-$outputIO = StringIO.new
-outputIO = StringIO.new
-$stdout = FakeStdout.new
-#{previous_commands}
-begin
-$stdout = FakeStdout.new
-result = instance_eval do
-#{line}
-end
-{:result => result, :output => $stdout.to_s}
-rescue SecurityError => e
-TryRubyOutput.standard(result: "SECURITY ERROR: " + e.inspect + e.backtrace.inspect)
-rescue Exception => e
-TryRubyOutput.error(error: e, output: outputIO.string)
-end
-EOF
-    line_count = 0
-    
-    # res = eval_cmd.lines.map do |line|
-    # line_count += 1
-    # line_count.to_s.ljust(4) + line
-    # end.join
-    # puts res
-      
- 
-    # o = Object.new
-    # thread = Thread.new { o.instance_eval(eval_cmd) }
-    # result = thread.value
-    eval_result = eval(eval_cmd)
-    $stdout = $original_stdout
-    return eval_result if eval_result.is_a?(TryRubyOutput) # exception occurred
-    output = eval_result[:output]
-    result = eval_result[:result]
- 
- 
-    # result = eval(eval_cmd)
-    session.past_commands << line
-    if result.is_a?(JavascriptResult) then
-      return TryRubyOutput.javascript(javascript: result.javascript, output: output)
-    else
-      return TryRubyOutput.standard(result: result, output: output)
-    end
- 
-  end
-  
-end
