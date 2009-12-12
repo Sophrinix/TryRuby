abff334a4b7010f10b604b00941775e7191cbf6d
David Miani <davidmiani@gmail.com>
2009-12-12 17:58:04 +1000

    added code to create sample file for tryruby lessons

--- public/tryruby_runner.rb
+++ public/tryruby_runner.rb
@@ -118,6 +118,43 @@ end
 
 FakeFS.activate!
 FakeFS::FileSystem.clear
+
+# create the basic filesystem
+["Home", "Libraries", "MouseHole", "Programs", "Tutorials"].each {|dir| Dir.mkdir dir}
+comic_txt_text = <<COMIC_EOF
+Achewood: http://achewood.com/                           
+Dinosaur Comics: http://qwantz.com/                      
+Perry Bible Fellowship: http://cheston.com/pbf/archive.html
+Get Your War On: http://mnftiu.cc/  
+COMIC_EOF
+File.open("/comics.txt", "w") { |file| file << file.write(comic_txt_text) }
+
+flickrpedia_user_rb_text = <<FLICK_EOF
+MouseHole.script do                                                   
+  name 'FlickrPedia'                                                  
+  namespace 'mail@robertbrook.com'                                    
+  description "Inserts Wikipedia links for Flickr tags"               
+  version '0.1'                                                       
+                                                                      
+  include_match %r{^HTTP://www.flickr.com/photos/.*?$}                
+                                                                      
+  rewrite do |req, res|                                               
+    document.elements.each('//a[@class="globe"]') do |link|           
+      href = link.attributes['href']                                  
+      clipped = href[13 ..-11] # who needs regex?                     
+      span = REXML::Element.new 'a'                                   
+      span.attributes['class'] = 'Grey'                               
+      span.attributes['href'] = 'HTTP://www.wikipedia.org/wiki/' + clipp
+ed                                                                    
+      span.text = 'W ' # this is pretty crappy...                     
+      link.parent.insert_after link, span                             
+    end                                                               
+  end                                                                 
+end  
+FLICK_EOF
+
+File.open("/MouseHole/flickrpedia.user.rb", "w") {|f| f.write(flickrpedia_user_rb_text)}
+
 $SAFE = 3
 $stdout = FakeStdout.new
 #{self.past_commands.join("\n")}
