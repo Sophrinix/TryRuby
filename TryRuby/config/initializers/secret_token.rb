2f8401ff0b4707a0a1e65c0bea7fe5b8d0c08508
Andrew McElroy <sophrinix@gmail.com>
2011-07-08 17:23:44 -0500

    trying to clean up

--- TryRuby/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TryRuby::Application.config.secret_token = 'f256893c2715b8fab149294c7f3b79fbc7a725534be51332c220c2e19965e631cc72c360acfec77dcc0469f58b81b5023e7c93a800626520f9420816639948d5'
